46b99d5cd3239f6c83b113f4565cb78a8a36684e
Willer César de Freitas <willercf@gmail.com>
2013-10-07 17:41:57 -0300

    adicção dos bundle de mensagens.

---
+++ src/main/java/br/fpu/tcc/hotelaria/web/ViewConstants.java
@@ -0,0 +1,8 @@
+package br.fpu.tcc.hotelaria.web;
+
+public class ViewConstants {
+
+	public static final String FUNCIONARIO_CADASTRO_SUCESSO = "funcionario.cadastro.sucesso";
+
+	public static final String FUNCIONARIO_CADASTRO_ERRO = "funcionario.cadastro.erro";
+}
