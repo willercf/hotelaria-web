e833c6dd6df53de4bbeef84929b91915bce01899
willercf <willercf@gmail.com>
2013-10-27 15:25:42 -0200

    checkin e checkout

---
+++ src/main/java/br/fpu/tcc/hotelaria/pojo/CheckIn.java
@@ -0,0 +1,46 @@
+package br.fpu.tcc.hotelaria.pojo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+//@Entity
+//@Table(name = "tb_checkin")
+public class CheckIn implements Serializable {
+
+//	/**
+//	 *
+//	 */
+//	private static final long serialVersionUID = -1776091711198221411L;
+//	private long id_checkin;
+//
+//	private Reserva reserva;
+//
+//	@Id
+//	@GeneratedValue
+//	@Column(name = "id_checkin")
+//	public long getId_checkin() {
+//		return id_checkin;
+//	}
+//
+//	public void setId_checkin(long id_checkin) {
+//		this.id_checkin = id_checkin;
+//	}
+//
+//	@OneToOne
+//	@JoinColumn(name = "reserva", referencedColumnName = "id_reserva")
+//	public Reserva getReserva() {
+//		return reserva;
+//	}
+//
+//	public void setReserva(Reserva reserva) {
+//		this.reserva = reserva;
+//	}
+
+}
---
+++ src/main/java/br/fpu/tcc/hotelaria/pojo/CheckOut.java
@@ -0,0 +1,45 @@
+package br.fpu.tcc.hotelaria.pojo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+//@Entity
+//@Table(name = "tb_checkout")
+public class CheckOut implements Serializable {
+
+//	/**
+//	 *
+//	 */
+//	private static final long serialVersionUID = -1491368704818335305L;
+//	private long id_checkout;
+//	private Reserva reserva;
+//
+//	@Id
+//	@GeneratedValue
+//	@Column(name = "id_checkout")
+//	public long getId_checkout() {
+//		return id_checkout;
+//	}
+//
+//	public void setId_checkout(long id_checkout) {
+//		this.id_checkout = id_checkout;
+//	}
+//
+//	@OneToOne
+//	@JoinColumn(name = "reserva", referencedColumnName = "id_reserva")
+//	public Reserva getReserva() {
+//		return reserva;
+//	}
+//
+//	public void setReserva(Reserva reserva) {
+//		this.reserva = reserva;
+//	}
+
+}
